c2e2c9930485f759c0062616290a665c1345e883
Brian Lynch <brianlynch@Brians-MacBook-Pro.local>
2018-08-06 10:24:36 -0400

    fixed functionality need to style

--- clicky-app/src/App.js
+++ clicky-app/src/App.js
@@ -5,81 +5,80 @@ import Title from "./components/Title";
 import friends from "./buttons.json";
 import "./App.css";
 
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
+
 
 class App extends Component {
   state = {
-    friends,
+    friends: friends,
     score: 0,
     topScore: 0,
     clickedFriends: [],
     isCorrect: ""
   };
 
+  shuffle = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
 
   handleClick = id => {
-    if (this.state.clickedFriend.indexOf(id) === -1) {
-      this.handleIncrement();
-      this.setState({ clickedFriend: this.state.clickedFriend.concat(id) });
+    if (this.state.clickedFriends.indexOf(id) === -1) {
+      this.setState({
+        // Shuffles the friends json I have imported and returns the newly shuffled array
+        friends: this.shuffle(this.state.friends),
+        // Takes the previous score and adds one
+        score: this.state.score + 1,
+        // Compares the current top score to the current score ON CLICK
+        topScore: (this.state.score + 1 > this.state.topScore) ? this.state.score + 1 : this.state.topScore,
+        // if(this.state.score + 1 > this.state.topScore) {
+        //   return this.state.score + 1
+        // } else {
+        //   return this.state.topScore
+        // }
+        clickedFriends: [...this.state.clickedFriends, id],
+        // let array = [1,2,3,4,5]
+        // array.push(6)
+        // // Expected outcome of [1,2,3,4,5,6]
+        // array = [...array, 6]
+        // // Expected outcome of [1,2,3,4,5,6]
+        // isCorrect: "You clicked right!"
+      });
     } else {
-      this.handleReset();
+      this.setState({
+        friends: this.shuffle(this.state.friends),
+        score: 0,
+        topScore: (this.state.score + 1 > this.state.topScore) ? this.state.score + 1 : this.state.topScore,
+        clickedFriends: [],
+        isCorrect: "You lose! Try again?"
+      });
     }
-  };
-
-  handleIncrement = () => {
-    const updateScore = this.state.score + 1
-    this.setState({
-      score: updateScore,
-      isCorrect: ""
-    });
-    if (updateScore >= this.state.topScore) {
-      this.setState({ topScore: updateScore });
-    }
-    else if (updateScore === 12) {
-      this.setState({ isCorrect: "Winner!" })
-    }
-    this.makeShuffle();
-  };
-
-  handleReset = () => {
-    this.setState({
-      currentScore: 0,
-      topScore: this.state.topScore,
-      clicked: [],
-      isCorrect: "reset game"
-    });
-    this.makeShuffle();
-  };
-
-
-  makeShuffle = () => {
-    let shuffledCards = shuffle(friends);
-    this.setState({ friends: shuffledCards });
   };
 
   render() {
-    const shuffledCards = shuffle(this.state.friends);
+    // state = {
+    //   friends: friends,
+    //   score: 0,
+    //   topScore: 0,
+    //   clickedFriends: [],
+    //   isCorrect: ""
+    // };
+    const { friends, score, topScore, isCorrect } = this.state;
+
     return (
       <Wrapper>
         <Title>Friends List
-          score={this.state.score}
-          topScore={this.state.topScore}
-          isCorrect={this.state.isCorrect}
+          score={score}
+          topScore={topScore}
+          isCorrect={isCorrect}
         </Title>
 
-        {this.state.friends.map(friend => (
+        {friends.map(friend => (
           <FriendCard
             key={friend.id}
             handleClick={this.handleClick}
-            handleIncrement={this.handleIncrement}
-            handleReset={this.handleReset}
-            makeShuffle={this.makeShuffle}
             id={friend.id}
             image={friend.image}
           />
